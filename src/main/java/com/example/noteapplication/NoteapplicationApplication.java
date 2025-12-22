2849f2494297373dde50265d15af2bddeb13eebe
deca <idrisadejumo@gmail.com>
2025-12-22 09:22:19 +0100

    initial commit

---
+++ src/main/java/com/example/noteapplication/NoteapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.example.noteapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteapplicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoteapplicationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/noteapplication/NoteapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.noteapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteapplicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
